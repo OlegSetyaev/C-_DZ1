a08d9df67b1227f296252d316e579a022696422b
Oleg <FireResist88@mail.ru>
2022-11-21 13:14:00 +0300

    added task1

---
+++ Seminar1_DZ/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа для поиска наибольшего из двух чисел.
+Console.WriteLine ("Введите первое число: ");
+int FirstNumber = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите второе число: ");
+int SecondNumber = Convert.ToInt32 (Console.ReadLine());
+if (FirstNumber > SecondNumber)
+    Console.WriteLine ("Наибольшее число "+FirstNumber);
+else 
+    Console.WriteLine ("Наибольшее число "+SecondNumber);
