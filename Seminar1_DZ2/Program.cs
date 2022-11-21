a14e3e2ff78eeff2b3052bc699a8dea74bf00c9a
Oleg <FireResist88@mail.ru>
2022-11-21 13:39:44 +0300

    addet task2

---
+++ Seminar1_DZ2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа которая выдаёт максимальное из 3х чисел
+Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int thirdNumber = Convert.ToInt32 (Console.ReadLine());
+int max = (firstNumber);
+if (max < secondNumber)
+    max = secondNumber;
+if (max < thirdNumber)
+    max = thirdNumber;
+Console.WriteLine(max);
