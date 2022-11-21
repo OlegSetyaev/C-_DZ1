7e3740469fa6df957b4c443735121f05005e9a8f
Oleg <FireResist88@mail.ru>
2022-11-21 15:14:37 +0300

    added task 4

---
+++ Seminar1_DZ4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа которая принимает число N, а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+for (i = 1; i <= n; i++)
+    if  (i % 2 == 0) {
+        Console.Write($"{i}");
+       if ( i + 1 < n)
+       Console.Write(",");
+    }
+    else if (i==n)
+        Console.WriteLine();
