52de08f7591eca08912480a2ebaccd6f5c5e0da8
Oleg <FireResist88@mail.ru>
2022-11-21 14:01:20 +0300

    added task 3

---
+++ Seminar1_DZ3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа которая на вход принимает целое число и выдаёт, является число чётным.
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+if ((number % 2) == 1)
+    Console.WriteLine(number + "->NO");
+else if ((number % 2) == 0)
+    Console.WriteLine(number + "->YES");
